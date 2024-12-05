1d37d2e636c34f4501c73ba52c17b0fb30db903d
ryandcunha25 <ryandcunha2812@gmail.com>
2024-12-05 17:23:52 +0530

    added font style and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", // Scan all React component files
-    "./public/index.html",],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif",
+      },
+      colors:{
+        primary: "#FD3D57",
+        grey : "#D3D3D3"
+      },
+      backgroundImage: {
+        'test-img': "url('../public/images/jordan_homescreen.webp')",
+      }
+    },
   },
   plugins: [],
-}
+};
 
+
+// 
