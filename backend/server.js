2ceee89d5a050cb961f859276389bff509d617e6
ryandcunha25 <ryandcunha2812@gmail.com>
2024-12-06 21:42:11 +0530

    added route for venues

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const pool = require("./db")
 const authRoutes = require('./routes/authentication');
+const venueRoutes = require("./routes/venues"); 
+
 pool.connect();
 
 const app = express();
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/authentication', authRoutes);
+
+app.use("/venues", venueRoutes);
 
 app.listen(5000, () => {
   console.log('\nServer is running on http://localhost:3000');
