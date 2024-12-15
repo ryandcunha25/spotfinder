4f2dc8c814d1b97d60a36447665d29390c342117
ryandcunha25 <ryandcunha2812@gmail.com>
2024-12-15 21:37:27 +0530

    added route for venue details

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LoginPage from './Components/LoginPage';
 import HomePage from './Components/HomePage';
 import Navbar from './Components/Navbar';
 import Spots from './Components/Spots';
+import VenueDetails from './Components/VenueDetails';
 
 function AppContent() {
   const location = useLocation();
@@ -24,6 +25,7 @@ function AppContent() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/homepage" element={<HomePage />} />
           <Route path="/spots" element={<Spots />} />
+          <Route path="/venues/:venueId" element={<VenueDetails />} />
         </Routes>
       </div>
     </div>
