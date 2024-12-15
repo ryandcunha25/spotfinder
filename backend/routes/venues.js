57fc1781476454b8a67e600a074d8c45020b9613
ryandcunha25 <ryandcunha2812@gmail.com>
2024-12-15 21:35:30 +0530

    added route for venue details

--- backend/routes/venues.js
+++ backend/routes/venues.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db"); // Adjust the path based on your project structure
 
-// Endpoint to fetch venues
-router.get("/", async (req, res) => {
+// Fetching all venues
+router.get('/', async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM venues");
     res.json(result.rows);
@@ -13,5 +13,24 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Fetching individual venues
+router.get('/:venueId', async (req, res) => {
+  const { venueId } = req.params;
+  console.log('Showing details of venueId = :', venueId);
+
+  // Fetch the venue from your database
+  try {
+    const venue = await pool.query('SELECT * FROM venues WHERE venue_id = $1', [venueId]);
+    if (venue.rows.length === 0) {
+      return res.status(404).json({ error: 'Venue not found' });
+    }
+    res.json(venue.rows[0]);
+  } catch (error) {
+    console.error('Error fetching venue:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 module.exports = router;
- 
+
