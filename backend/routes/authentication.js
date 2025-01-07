c0ec234861e97e081115fd0ac611fcf1c06c5f9f
ryandcunha25 <ryandcunha2812@gmail.com>
2025-01-07 17:25:12 +0530

    no changes

--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -5,6 +5,7 @@ const pool = require('../db');
 const router = express.Router();
 require('dotenv').config();
 
+
 const SECRET_KEY = process.env.SECRET_KEY;
 
 // Sign Up
@@ -12,14 +13,14 @@ router.post('/signup', async (req, res) => {
     const { first_name, last_name, phone, email, password, cpassword } = req.body;
     if (password !== cpassword) {
         return res.status(400).json({ error: 'Passwords do not match!' });
-      }
+    }
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
         await pool.query(
             'INSERT INTO users (first_name, last_name, phone, email, password) VALUES ($1, $2, $3, $4, $5)',
             [first_name, last_name, phone, email, hashedPassword]
         );
-        console.log("\nNew User signed up -->",req.body);
+        console.log("\nNew User signed up -->", req.body);
         res.status(201).send('User registered successfully');
     } catch (err) {
         res.status(500).send('Error registering user');
@@ -29,19 +30,40 @@ router.post('/signup', async (req, res) => {
 // Login
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
+
     try {
         const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-        console.log("\nUser logging in -->",req.body);
-        if (user.rows.length === 0) return res.status(404).send('User not found');
+        console.log("\nUser logging in -->", req.body);
+
+        if (user.rows.length === 0) {
+            return res.status(404).send('User not found');
+        }
 
         const validPassword = await bcrypt.compare(password, user.rows[0].password);
-        if (!validPassword) return res.status(401).send('Invalid credentials');
+        if (!validPassword) {
+            return res.status(401).send('Invalid credentials');
+        }
 
-        const token = jwt.sign({ userId: user.rows[0].id }, SECRET_KEY, { expiresIn: '1h' });
-        res.status(200).json({ token });
+        // Extract the user ID and email
+        const userdetails = user.rows[0];
+        const userId = user.rows[0].id;
+        const userEmail = user.rows[0].email;
+
+        // Generate JWT with the user ID
+        const token = jwt.sign({ id: userId, email: userEmail }, process.env.JWT_SECRET, {
+            expiresIn: '1h', // Token validity
+        });
+
+        console.log('Generated Token:', token);
+
+        res.json({ message: 'Login successful', userdetails , token });
+
     } catch (err) {
+        console.error('Error during login:', err);
         res.status(500).send('Error logging in');
     }
 });
+
+
 
 module.exports = router;
