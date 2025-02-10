3e36913a034e8123e66e4fa08868472f72e7dad1
ryandcunha25 <ryandcunha2812@gmail.com>
2025-02-10 08:08:33 +0530

    formatted the code

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -23,7 +23,10 @@ const SignUp = () => {
 
     const sendOtp = async () => {
         try {
-            await axios.post('http://localhost:5000/authentication/send-otp', { email: formData.email });
+            await axios.post(
+                'http://localhost:5000/authentication/send-otp',
+                { email: formData.email }
+            );
             setIsOtpSent(true);
             alert('OTP sent to your email');
         } catch (err) {
@@ -33,7 +36,10 @@ const SignUp = () => {
 
     const verifyOtp = async () => {
         try {
-            const response = await axios.post('http://localhost:5000/authentication/verify-otp', { email: formData.email, otp });
+            const response = await axios.post(
+                'http://localhost:5000/authentication/verify-otp',
+                { email: formData.email, otp }
+            );
             if (response.status === 200) {
                 setIsVerified(true);
                 setIsOtpSent(false); // Hide OTP input field
@@ -59,7 +65,10 @@ const SignUp = () => {
             return;
         }
         try {
-            await axios.post('http://localhost:5000/authentication/signup', formData);
+            await axios.post(
+                'http://localhost:5000/authentication/signup',
+                formData
+            );
             alert('User registered successfully');
             navigate('/homepage');
 
@@ -69,21 +78,43 @@ const SignUp = () => {
     };
 
     return (
-        <div className="w-full bg-cover bg-blend-darken min-h-screen" style={{ backgroundImage: `url(${loginBg1})` }}>
+        <div
+            className="w-full bg-cover bg-blend-darken min-h-screen"
+            style={{ backgroundImage: `url(${loginBg1})` }}
+        >
             <section className="flex justify-center items-center min-h-screen w-full bg-center bg-cover">
-                <div className="relative w-[500px] bg-transparent border-2 border-white/50 rounded-2xl backdrop-blur-lg p-6">
-                    <form className="w-full" onSubmit={handleSubmit}>
+                <div
+                    className="relative w-[500px] bg-transparent border-2 border-white/50 rounded-2xl backdrop-blur-lg p-6"
+                >
+                    <form
+                        className="w-full"
+                        onSubmit={handleSubmit}
+                    >
                         <h2 className="text-2xl text-black text-center font-bold mb-6">Sign Up</h2>
 
                         {/* Row 1: First Name and Last Name */}
                         <div className="grid grid-cols-2 gap-4 mb-4">
                             <div>
                                 <label className="text-sm text-black block mb-2">First Name</label>
-                                <input type="text" required onChange={handleChange} name="first_name" placeholder="Enter first name" className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 outline-none placeholder-gray-100 text-black text-lg px-2 focus:ring-0 peer" />
+                                <input
+                                    type="text"
+                                    required
+                                    onChange={handleChange}
+                                    name="first_name"
+                                    placeholder="Enter first name"
+                                    className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 outline-none placeholder-gray-100 text-black text-lg px-2 focus:ring-0 peer"
+                                />
                             </div>
                             <div>
                                 <label className="text-sm text-black block mb-2">Last Name</label>
-                                <input type="text" required onChange={handleChange} name="last_name" placeholder="Enter last name" className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 outline-none placeholder-gray-100 text-black text-lg px-2 focus:ring-0 peer" />
+                                <input
+                                    type="text"
+                                    required
+                                    onChange={handleChange}
+                                    name="last_name"
+                                    placeholder="Enter last name"
+                                    className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 outline-none placeholder-gray-100 text-black text-lg px-2 focus:ring-0 peer"
+                                />
                             </div>
                         </div>
 
@@ -91,14 +122,28 @@ const SignUp = () => {
                         <div className="grid grid-cols-2 gap-4 mb-4">
                             <div>
                                 <label className="text-sm text-black block mb-2">Email</label>
-                                <input type="email" required onChange={handleChange} name="email" placeholder="Enter email" className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 outline-none placeholder-white text-black text-lg px-2 placeholder-gray-100 focus:ring-0 peer" />
+                                <input
+                                    type="email"
+                                    required
+                                    onChange={handleChange}
+                                    name="email"
+                                    placeholder="Enter email"
+                                    className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 outline-none placeholder-white text-black text-lg px-2 placeholder-gray-100 focus:ring-0 peer"
+                                />
                             </div>
                             <div>
                                 <label className="text-sm text-black block mb-2">Phone Number</label>
-                                <input type="number" required onChange={handleChange} name="phone" placeholder="Enter phone number" className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 outline-none placeholder-white text-black text-lg px-2 focus:ring-0 peer"
-                                    pattern="[0-9]*" // Allows only numbers
-                                    inputMode="numeric" // Helps mobile keyboards show number keypad
-                                    maxLength="10" />
+                                <input
+                                    type="number"
+                                    required
+                                    onChange={handleChange}
+                                    name="phone"
+                                    placeholder="Enter phone number"
+                                    className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 outline-none placeholder-white text-black text-lg px-2 focus:ring-0 peer"
+                                    pattern="[0-9]*"
+                                    inputMode="numeric"
+                                    maxLength="10"
+                                />
                             </div>
                         </div>
 
@@ -106,38 +151,79 @@ const SignUp = () => {
                         {!isVerified && isOtpSent ? (
                             <div className="mb-4">
                                 <label className="text-sm text-black block mb-2">Enter OTP</label>
-                                <input type="text" required onChange={(e) => setOtp(e.target.value)} name="otp" placeholder="Enter OTP" className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 placeholder-white outline-none text-black text-lg px-2 focus:ring-0 peer" />
-                                <button type="button" onClick={verifyOtp} className="w-full h-10 rounded-full bg-blue-500 text-white font-semibold text-lg hover:bg-blue-700 mt-2">Verify OTP</button>
+                                <input
+                                    type="text"
+                                    required
+                                    onChange={(e) => setOtp(e.target.value)}
+                                    name="otp"
+                                    placeholder="Enter OTP"
+                                    className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 placeholder-white outline-none text-black text-lg px-2 focus:ring-0 peer"
+                                />
+                                <button
+                                    type="button"
+                                    onClick={verifyOtp}
+                                    className="w-full h-10 rounded-full bg-blue-500 text-white font-semibold text-lg hover:bg-blue-700 mt-2"
+                                >
+                                    Verify OTP
+                                </button>
                             </div>
                         ) : (
                             !isVerified && (
-                                <button type="button" onClick={sendOtp} className="w-full h-10 rounded-full bg-blue-500 text-white font-semibold text-lg hover:bg-blue-700 mb-4">Send OTP</button>
+                                <button
+                                    type="button"
+                                    onClick={sendOtp}
+                                    className="w-full h-10 rounded-full bg-blue-500 text-white font-semibold text-lg hover:bg-blue-700 mb-4"
+                                >
+                                    Send OTP
+                                </button>
                             )
                         )}
 
-                        {/* Password and Confirm Password Fields (Visible only after OTP verification) */}
+                        {/* Password and Confirm Password Fields */}
                         {isVerified && (
                             <div className="grid grid-cols-2 gap-4 mb-6">
                                 <div>
                                     <label className="text-sm text-black block mb-2">Password</label>
-                                    <input type="password" required onChange={handleChange} name="password" placeholder="Enter password" className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 outline-none placeholder-white text-black text-lg px-2 focus:ring-0 peer" />
+                                    <input
+                                        type="password"
+                                        required
+                                        onChange={handleChange}
+                                        name="password"
+                                        placeholder="Enter password"
+                                        className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 outline-none placeholder-white text-black text-lg px-2 focus:ring-0 peer"
+                                    />
                                 </div>
                                 <div>
                                     <label className="text-sm text-black block mb-2">Confirm Password</label>
-                                    <input type="password" required onChange={handleChange} name="cpassword" placeholder="Confirm password" className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 outline-none placeholder-white text-black text-lg px-2 focus:ring-0 peer" />
+                                    <input
+                                        type="password"
+                                        required
+                                        onChange={handleChange}
+                                        name="cpassword"
+                                        placeholder="Confirm password"
+                                        className="w-full h-[50px] bg-transparent border-b-2 border-gray-200 outline-none placeholder-white text-black text-lg px-2 focus:ring-0 peer"
+                                    />
                                 </div>
                             </div>
                         )}
 
                         {isVerified && (
                             <div className="text-center">
-                                <button type="submit" className="w-full h-10 rounded-full bg-white text-gray-800 font-semibold text-lg hover:bg-gray-200 px-6">Sign Up</button>
+                                <button
+                                    type="submit"
+                                    className="w-full h-10 rounded-full bg-white text-gray-800 font-semibold text-lg hover:bg-gray-200 px-6"
+                                >
+                                    Sign Up
+                                </button>
                             </div>
                         )}
-
-                        <div className="text-center text-white text-md mt-6">
-                            <p>Already have an account? <Link to="/" className="font-semibold hover:underline">Login</Link></p>
-                            <p>Are you a venue owner? <Link to="/venueownersregistration" className="font-semibold hover:underline">Sign up as one!</Link></p>
+                        <div className="text-center text-white text-sm mt-4">
+                            <p>Don't have an account? 
+                                <Link to="/signup" className="font-semibold hover:underline"> Sign Up!</Link>
+                            </p>
+                            <p>Wanna sign up as an venue owner? 
+                                <Link to="/venueownersregistration" className="font-semibold hover:underline"> Register</Link>
+                            </p>
                         </div>
                     </form>
                 </div>
