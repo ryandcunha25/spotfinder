6553bc398737ad53cc1cf51ffb58b2aedf219f65
ryandcunha25 <ryandcunha2812@gmail.com>
2025-08-23 00:06:58 +0530

    added message imports from antd

--- src/Components/Users/Profile/Accounts.jsx
+++ src/Components/Users/Profile/Accounts.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import { message } from 'antd';
 import ProfileSidebar from './ProfileSidebar';
 import ProfileInfo from './ProfileInfo';
 import EditProfile from './EditProfile';
--- src/Components/Users/UserBookings.jsx
+++ src/Components/Users/UserBookings.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { message } from "antd";
 
 const BookingsList = () => {
   const [bookings, setBookings] = useState({
